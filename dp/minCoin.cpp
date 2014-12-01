9029f5b6c84c5b2705b7aac16956d7b1f3f3dfb5
baoxue <baoxue.zhao@gmail.com>
2014-12-01 13:51:56 +0800

    Create minCoin.cpp

---
+++ dp/minCoin.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int minCoin(int A[], int k, int n)
+{
+	int dp[k+1][n+1];
+	int prev[k+1][n+1][2];
+
+	memset(dp, 0, sizeof(dp));
+	memset(prev, 0, sizeof(prev));
+
+	dp[1][1] = 1;
+	prev[1][1][0] = 1;
+	prev[1][1][1] = 1;
+
+	for(int j=2; j<=n; j++)
+	{
+		dp[1][j] = j;
+		prev[1][j][0] = 1;
+		prev[1][j][0] = j-1;
+	}
+
+	for(int i=1; i<=k; i++)
+	{
+		dp[i][0] = 0;
+	}
+
+	for(int i=2; i<=k; i++)
+	{
+		for(int j=1; j<=n; j++)
+		{
+			dp[i][j] = dp[i-1][j];
+			
+			prev[i][j][0] = i-1;
+			prev[i][j][1] = j;
+
+			if(j >= A[i-1])
+			{
+				if(dp[i][j-A[i-1]]+1 < dp[i][j])
+				{
+					dp[i][j] = dp[i][j-A[i-1]]+1;
+					prev[i][j][0] = i;
+					prev[i][j][1] = j-A[i-1];
+				}
+			}
+		}
+	}
+
+	int i = k;
+	int j = n;
+	int remain = n;
+
+	while(i != prev[i][j][0] || j != prev[i][j][1])
+	{
+		cout << i << "\t" << j << endl;
+		/*
+		if(j != prev[i][j][1])
+		{
+			cout << j-prev[i][j][1] << " ";
+		}*/
+		i = prev[i][j][0];
+		j = prev[i][j][1];
+	}
+	cout << endl;
+
+	return dp[k][n];
+}
+
+int main()
+{
+	int A[] = {1, 5, 8};
+
+	for(int i=26; i<=26; i++)
+		cout << i << "\t" << minCoin(A, 3, i) << endl;;
+	return 0;
+}

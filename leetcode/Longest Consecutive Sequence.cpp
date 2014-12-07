9b2cc6779d6366596a46d1b34e97a86d2601b09c
baoxue <baoxue.zhao@gmail.com>
2014-12-07 17:27:20 +0800

    Create Longest Consecutive Sequence.cpp

---
+++ leetcode/Longest Consecutive Sequence.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int> &num) {
+        unordered_map<int, bool> hash;
+        
+        for(int i=0; i<num.size(); i++)
+        {
+            hash[num[i]] = true;
+        }
+        
+        int max_len = -1;
+        for(auto p: hash)
+        {
+            if(!p.second) continue;
+            
+            int left = p.first-1;
+            int len = 1;
+            while(hash[left])
+            {
+                hash[left] = false;
+                left--;
+                len++;
+            }
+            
+            int right = p.first+1;
+            while(hash[right])
+            {
+                hash[right] = false;
+                right++;
+                len++;
+            }
+            max_len = max(max_len, len);
+        }
+        return max_len;
+    }
+};

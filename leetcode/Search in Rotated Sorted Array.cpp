4deea5df926e42bdd9f12a3f2f1e1d25a6ac49b6
baoxue <baoxue.zhao@gmail.com>
2014-12-08 15:38:37 +0800

    Create Search in Rotated Sorted Array.cpp

---
+++ leetcode/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int search(int A[], int n, int target) {
+        int left = 0, right = n-1;
+        while(left <= right)
+        {
+            int mid = left+(right-left)/2;
+            if(A[mid] == target)
+                return mid;
+            
+            if(A[mid] < A[right])
+            {
+                if(target > A[mid] && target <= A[right])
+                    left = mid+1;
+                else
+                    right = mid-1;
+            }
+            else
+            {
+                if(target >= A[left] && target < A[mid])
+                    right = mid-1;
+                else
+                    left = mid+1;
+            }
+        }
+        return -1;
+    }
+};

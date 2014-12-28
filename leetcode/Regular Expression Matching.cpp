6d2c11f93a0ec92e6b81ca3d4d7f5eeca3ca17fb
baoxue <baoxue.zhao@gmail.com>
2014-12-28 15:57:47 +0800

    Update Regular Expression Matching.cpp

--- leetcode/Regular Expression Matching.cpp
+++ leetcode/Regular Expression Matching.cpp
@@ -24,3 +24,37 @@ public:
         return false;
     }
 };
+
+
+//dynamic programming
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+            
+        if (*p == 0) return *s == 0;  //empty
+        
+        const int n = strlen(s);
+        const int m = strlen(p);
+        
+        bool dp[n+1][m+1];
+        memset(dp, false, sizeof(dp));
+        
+        dp[0][0] = true;
+        
+        for(int i=1; i<=m; i++)
+        {
+            dp[0][i] = (i>=2 && p[i-1] == '*') && dp[0][i-2];
+        }
+        
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j <= m; ++j)
+            {
+                dp[i][j] = dp[i][j] || ((s[i-1] == p[j-1] || p[j-1] == '.') && dp[i-1][j-1]);
+                dp[i][j] = dp[i][j] || (j-2 >= 0 && p[j-1] == '*' && dp[i][j-2]);
+                dp[i][j] = dp[i][j] || (j-2 >= 0 && p[j-1] == '*' && (s[i-1] == p[j-2] || p[j-2] == '.') && dp[i-1][j]);
+            }
+        }
+        return dp[n][m];
+    }
+};

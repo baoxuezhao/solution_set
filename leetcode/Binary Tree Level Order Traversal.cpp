bfcd8b67af50d138b555858475d4b865ba8ef4cd
baoxue <baoxue.zhao@gmail.com>
2014-12-29 18:14:45 +0800

    Update Binary Tree Level Order Traversal.cpp

--- leetcode/Binary Tree Level Order Traversal.cpp
+++ leetcode/Binary Tree Level Order Traversal.cpp
@@ -24,3 +24,40 @@ public:
         return max(getHeight(root->left), getHeight(root->right))+1;
     }
 };
+
+//DFS solution 2, each node is traversed multiple times.
+class Solution {
+public:
+    vector<vector<int> > levelOrder(TreeNode *root) {
+        int height = getHeight(root);
+        vector<vector<int> > res;
+        for(int level = 1; level <= height; level++)
+        {
+            vector<int> vec;
+            printLevel(root, level, vec);
+            res.push_back(vec);
+        }
+        return res;
+    }
+    
+    void printLevel(TreeNode *root, int level, vector<int> &vec)
+    {
+        if(root == NULL) return;
+        if(level == 1)
+        {
+            vec.push_back(root->val);
+        }
+        else
+        {
+            printLevel(root->left, level-1, vec);
+            printLevel(root->right, level-1, vec);
+        }
+    }
+    
+    int getHeight(TreeNode *root)
+    {
+        if(root == NULL)
+            return 0;
+        return max(getHeight(root->left), getHeight(root->right))+1;
+    }
+};

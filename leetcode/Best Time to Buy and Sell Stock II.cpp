3c7f1f24188063ca91eceb25f292a13ad431a540
baoxue <baoxue.zhao@gmail.com>
2014-12-31 20:34:22 +0800

    Create Best Time to Buy and Sell Stock II.cpp

---
+++ leetcode/Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        if(prices.size() < 2) return 0;
+        
+        int sum = 0;
+        
+        for(int i=0; i<prices.size()-1; i++)
+        {
+            if(prices[i+1]-prices[i] > 0)
+            {
+                sum += (prices[i+1]-prices[i]);
+            }
+        }
+        return sum;
+    }
+};

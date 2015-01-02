695cb6c919f1928fc6b5c64eb565af90ab5b3626
baoxue <baoxue.zhao@gmail.com>
2015-01-02 20:49:16 +0800

    Create Swap Nodes in Pairs.cpp

---
+++ leetcode/Swap Nodes in Pairs.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *swapPairs(ListNode *head) {
+        if(head == NULL || head->next == NULL)
+            return head;
+        
+        ListNode dummy(-1);
+        dummy.next = head;
+        
+        ListNode *prev = &dummy, *cur = head, *next = head->next;
+        while(cur && next)
+        {
+            prev->next = next;
+            cur->next = next->next;
+            next->next = cur;
+            
+            prev = cur;
+            cur = cur->next;
+            next = cur?cur->next:NULL;
+        }
+        return dummy.next;
+    }
+};

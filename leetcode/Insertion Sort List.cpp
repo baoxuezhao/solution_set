985ed1c063f8d1ed51113063be7640d4d6632bb5
baoxue <baoxue.zhao@gmail.com>
2015-01-02 17:13:16 +0800

    Create Insertion Sort List.cpp

---
+++ leetcode/Insertion Sort List.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    ListNode *insertionSortList(ListNode *head) {
+        if(head == NULL || head->next == NULL) return head;
+        
+        ListNode dummy(INT_MIN);
+        dummy.next = head;
+        ListNode *prev = &dummy, *cur = head;
+        
+        while(cur)
+        {
+            while(cur && cur->val >= prev->val)
+            {
+                prev = cur;
+                cur = cur->next;
+            }
+            if(cur)
+            {
+                prev->next = cur->next;
+                ListNode *t = &dummy, *p;
+                while(t && t->val <= cur->val) //??use <= in case the original list has INT_MIN value
+                {
+                    p = t;
+                    t = t->next;
+                }
+                p->next = cur;
+                cur->next = t;
+                
+                cur = prev->next;
+            }
+        }
+        return dummy.next;
+    }
+};

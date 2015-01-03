1a4db4e8417a46bdd0e546a22d9313ede4f93cd5
baoxue <baoxue.zhao@gmail.com>
2015-01-03 14:36:06 +0800

    Update Subsets.cpp

--- leetcode/Subsets.cpp
+++ leetcode/Subsets.cpp
@@ -1,3 +1,4 @@
+//binary bit map
 class Solution {
 public:
     vector<vector<int> > subsets(vector<int> &S) {
@@ -14,6 +15,26 @@ public:
             }
             res.push_back(sub);
             sub.clear();
+        }
+        return res;
+    }
+};
+
+//Solution incremental construct
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        sort(S.begin(), S.end());
+        vector<vector<int> > res(1);
+        
+        for(int i=0; i<S.size(); i++)
+        {
+            for(int j=res.size()-1; j>=0; j--)
+            {
+                vector<int> t(res[j].begin(), res[j].end());
+                t.push_back(S[i]);
+                res.push_back(t);
+            }
         }
         return res;
     }

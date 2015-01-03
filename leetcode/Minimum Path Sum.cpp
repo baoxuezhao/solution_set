e5f1e27ec88a61a3b6faa270d58168f9d9671171
baoxue <baoxue.zhao@gmail.com>
2015-01-03 20:08:12 +0800

    Create Minimum Path Sum.cpp

---
+++ leetcode/Minimum Path Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int> > &grid) {
+        int m = grid.size(), n = grid[0].size();
+        
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i == 0 && j == 0) continue;
+                
+                grid[i][j] += min(i>0?grid[i-1][j]:INT_MAX, j>0?grid[i][j-1]:INT_MAX);
+            }
+        }
+        return grid[m-1][n-1];
+    }
+};

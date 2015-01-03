23b1c9e2e3aaf41db4d19a439275a378db1deb46
baoxue <baoxue.zhao@gmail.com>
2015-01-03 11:31:14 +0800

    Create Gray Code.cpp

---
+++ leetcode/Gray Code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) 
+    {
+        vector<int> res(1, 0);
+        for(int i=1; i<=n; i++)    
+        {
+            for(int j=res.size()-1; j>=0; j--)
+            {
+                res.push_back(res[j] | (1 << (i-1)));
+            }
+        }
+        return res;
+    }
+};

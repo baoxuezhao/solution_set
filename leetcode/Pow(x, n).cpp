9a043b07ee409d2354fd3ee6658824f1ec754872
baoxue <baoxue.zhao@gmail.com>
2015-01-04 15:47:26 +0800

    Update Pow(x, n).cpp

--- leetcode/Pow(x, n).cpp
+++ leetcode/Pow(x, n).cpp
@@ -3,19 +3,18 @@
 class Solution {
 public:
     double pow(double x, int n) {
-        if(n < 0) 
-            return 1.0/power(x, 0-n);
-        else
-            return power(x, n); 
+        return power(x, n);
     }
     
-    double power(double x, int n)
+    double power(double x, long long n)
     {
+        if(n < 0) return 1.0/power(x, 0-n);
         if(n == 0) return 1;
-        double p = power(x, n/2);
-        if(n % 2 == 0)
-            return p*p;
+        
+        double half = power(x, n/2);
+        if(n%2 == 0)
+            return half*half;
         else
-            return p*p*x;
+            return half*half*x;
     }
 };

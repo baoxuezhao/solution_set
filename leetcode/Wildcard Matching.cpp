c3c704a1214ba20b3e98826cf1f84dc6a728b766
baoxue <baoxue.zhao@gmail.com>
2015-01-11 12:16:54 +0800

    Update Wildcard Matching.cpp

--- leetcode/Wildcard Matching.cpp
+++ leetcode/Wildcard Matching.cpp
@@ -30,3 +30,50 @@ public:
         return (*s == 0 && *p == 0);
     }
 };
+
+
+//the dp solution
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        int m = strlen(p);
+        int n = strlen(s);
+        
+        //the edge case must be processed, otherwise the dp solution will be timeout
+        int pl = 0;  
+        for (int i = 0; i < m; i++) {  
+            if (p[i] != '*') {  
+                pl++;  
+            }
+        }
+        if (pl > n) {  
+            return false;  
+        }  
+        if (n == 0 && pl == 0) {  
+            return true;  
+        }
+           
+        bool dp0[n+1];
+        bool dp1[n+1];
+        memset(dp0, false, sizeof(dp0));
+        memset(dp1, false, sizeof(dp1));
+        
+        dp0[0] = true;
+        
+        for(int i=1; i<=m; i++)
+        {
+            dp1[0] = (dp0[0] && p[i-1] == '*');
+            
+            for(int j=1; j<=n; j++)
+            {
+                if(p[i-1] == '?' || p[i-1]==s[j-1])
+                    dp1[j] = dp0[j-1];
+                else if(p[i-1] == '*')
+                    dp1[j] = (dp1[j-1] || dp0[j]);
+            }
+            memcpy(dp0, dp1, sizeof(dp1));
+            memset(dp1, false, sizeof(dp1));
+        }
+        return dp0[n];
+    }
+};

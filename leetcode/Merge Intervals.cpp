6ef6b8d03625750bd7420737e11b54af19adb024
baoxue <baoxue.zhao@gmail.com>
2015-01-21 20:37:57 -0800

    Update Merge Intervals.cpp

--- leetcode/Merge Intervals.cpp
+++ leetcode/Merge Intervals.cpp
@@ -47,3 +47,46 @@ public:
         return true;
     }
 };
+
+
+//////in-place solution
+bool comp(const Interval &in1, const Interval &in2)
+{
+    if(in1.start != in2.start)
+        return in1.start < in2.start;
+    return in1.end < in2.end;
+}
+
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval> &intervals) {
+        
+        if(intervals.size() == 0)
+            return intervals;
+        
+        std::sort(intervals.begin(), intervals.end(), comp);
+        
+        int j = 0;
+        for(int i=1; i<intervals.size(); i++)
+        {
+            if(overlap(intervals[j], intervals[i]))
+            {
+                intervals[j].start = min(intervals[j].start, intervals[i].start);
+                intervals[j].end   = max(intervals[j].end  , intervals[i].end);
+            }
+            else
+            {
+                intervals[++j] = intervals[i];
+            }
+        }
+        intervals.resize(j+1);
+        return intervals;
+    }
+    
+    bool overlap(const Interval &in1, const Interval &in2)
+    {
+        if(in1.start > in2.end || in2.start > in1.end)
+            return false;
+        return true;
+    }
+};

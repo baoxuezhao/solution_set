e06f482f839c8cc877eca7900f3c6817b938ba9c
baoxue <baoxue.zhao@gmail.com>
2015-02-08 14:54:12 +0800

    Update 4Sum.cpp

--- leetcode/4Sum.cpp
+++ leetcode/4Sum.cpp
@@ -47,3 +47,55 @@ public:
         return res;
     }
 };
+
+
+//general k-sum solution
+class Solution {
+public:
+    vector<vector<int> > fourSum(vector<int> &A, int target) {
+        sort(A.begin(), A.end());
+    	vector<vector<int> > res;
+    	vector<int> path;
+    	dfs(A, 0, 4, target, path, res);
+    	return res;
+    }
+    
+    void dfs(vector<int> &A, int idx, int k, int target, vector<int> &path, vector<vector<int> > &res)
+    {
+    	if(path.size() == k-2)
+    	{
+    		int left = idx, right = A.size()-1;
+    		while(left < right)
+    		{
+    			if(left>idx && A[left] == A[left-1]) 
+    				left++;
+    			else if(right<A.size()-1 && A[right] == A[right+1]) 
+    				right--;
+    			else
+    			{
+    				if(A[left] + A[right] == target)
+    				{
+    					res.push_back(path);
+    					res.back().push_back(A[left++]);
+    					res.back().push_back(A[right--]);
+    				}
+    				else if(A[left] + A[right] < target)
+    					left++;
+    				else
+    					right--;
+    			}
+    		}
+    	}
+    	else
+    	{
+    		for(int i=idx; i<A.size(); i++)
+    		{
+    			if(i>idx && A[i] == A[i-1]) continue;
+    
+    			path.push_back(A[i]);
+    			dfs(A, i+1, k, target-A[i], path, res);
+    			path.pop_back();
+    		}
+    	}
+    }
+};

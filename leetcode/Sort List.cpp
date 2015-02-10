a78763f3efc5dcd5d085d8b6f45a9a2e307dcaf9
baoxue <baoxue.zhao@gmail.com>
2015-02-10 13:55:45 +0800

    Update Sort List.cpp

--- leetcode/Sort List.cpp
+++ leetcode/Sort List.cpp
@@ -6,6 +6,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+ //solution 1, need to find the middle node at each round.
 class Solution {
 public:
     ListNode *sortList(ListNode *head) 
@@ -42,6 +43,68 @@ public:
                 cur->next = part2;
                 part2 = part2->next;
             }
+            cur = cur->next;
+        }
+        return dummy.next;
+    }
+};
+
+//solution 2, buttom up, without need to find the middle point at each round.
+class Solution {
+public:
+    ListNode *sortList(ListNode *head) {
+        if(head == NULL || head->next == NULL) return head;
+        int len = 0;
+        ListNode *cur = head;
+        while(cur)
+        {
+            len++;
+            cur = cur->next;
+        }
+        return sortutil(head, 0, len-1);
+    }
+    
+    ListNode *sortutil(ListNode *&head, int left, int right)
+    {
+        if(left > right) return NULL;
+        if(left == right)
+        {
+            ListNode *ret = head;
+            head = head->next;
+            ret->next = NULL;
+            return ret;
+        }
+        int mid = left+(right-left)/2;
+        
+        ListNode *lch = sortutil(head, left, mid);
+        ListNode *rch = sortutil(head, mid+1, right);
+        
+        ListNode dummy(-1);
+        ListNode *cur = &dummy;
+        while(lch && rch)
+        {
+            if(lch->val < rch->val)
+            {
+                cur->next = lch;
+                lch = lch->next;
+            }
+            else
+            {
+                cur->next = rch;
+                rch = rch->next;
+            }
+            cur = cur->next;
+        }
+        while(lch)
+        {
+            cur->next = lch;
+            lch = lch->next;
+            cur = cur->next;
+        }
+        while(rch)
+        {
+            cur->next = rch;
+            rch = rch->next;
             cur = cur->next;
         }
         return dummy.next;

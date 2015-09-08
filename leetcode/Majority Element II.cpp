1fbb5a8b4daaeb050a168af43abbaf6c239ad5b1
baoxue <baoxue.zhao@gmail.com>
2015-09-08 20:47:53 +0800

    Create Majority Element II.cpp

---
+++ leetcode/Majority Element II.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int num1 = INT_MAX;
+        int num2 = INT_MAX;
+        int count1 = 0;
+        int count2 = 0;
+        
+        for(int i=0; i<n; i++) {
+            if (nums[i] == num1) {
+                count1++;
+            } else if (nums[i] == num2) {
+                count2++;
+            } else if (count1 == 0) {
+                num1 = nums[i];
+                count1++;
+            } else if (count2 == 0) {
+                num2 = nums[i];
+                count2++;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0, count2 = 0;
+        for (int i=0; i<n; i++) {
+            if (nums[i] == num1) {
+                count1++;
+            } else if (nums[i] == num2) {
+                count2++;
+            }
+        }
+        vector<int> res;
+        if (count1 > n/3) res.push_back(num1);
+        if (count2 > n/3) res.push_back(num2);
+        return res;
+    }
+};

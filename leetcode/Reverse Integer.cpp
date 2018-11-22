68c851333a8a9df58b4efd4caa8c030e51848687
baoxue <baoxue.zhao@gmail.com>
2018-11-22 12:07:16 +0800

    Update Reverse Integer.cpp

--- leetcode/Reverse Integer.cpp
+++ leetcode/Reverse Integer.cpp
@@ -22,3 +22,23 @@ public:
         return res;
     }
 };
+
+// solution2
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            int y = x % 10;
+            x /= 10;
+            if (res > INT_MAX / 10 || (res == INT_MAX / 10 && y > 7)) {
+                return 0;
+            }
+            if (res < INT_MIN / 10 || (res == INT_MIN / 10 && y < -8)) {
+                return 0;
+            }
+            res = res * 10 + y;
+        }
+        return res;
+    }
+};

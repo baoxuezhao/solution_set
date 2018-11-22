30b46ff895ab9d8096592e0588cc430dd5a48dff
baoxue <baoxue.zhao@gmail.com>
2018-11-22 18:13:44 +0800

    Update Roman to Integer.cpp

--- leetcode/Roman to Integer.cpp
+++ leetcode/Roman to Integer.cpp
@@ -38,3 +38,30 @@ public:
         return res;
     }
 };
+
+// solution 2:
+class Solution {
+public:
+    int romanToInt(string s) {
+        const int radix[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        const string symbol[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int size = sizeof(radix)/sizeof(int);
+        
+        int n = s.size();
+        int idx = 0;
+        int res = 0;
+        while (idx < n) {
+            for (int i = 0; i < size; ++i) {
+                while (idx < n && symbol[i].size() == 1 && s[idx] == symbol[i][0]) {
+                    res += radix[i];
+                    idx++;
+                }
+                while (idx < n && symbol[i].size() == 2 && s.substr(idx, 2) == symbol[i])  {
+                    res += radix[i];
+                    idx+=2;
+                }
+            }
+        }
+        return res;
+    }
+};

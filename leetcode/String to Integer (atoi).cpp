a0907d0360c4d21ae4d22480bf5d7821f3530ce5
baoxue <baoxue.zhao@gmail.com>
2018-11-22 15:23:48 +0800

    Update String to Integer (atoi).cpp

--- leetcode/String to Integer (atoi).cpp
+++ leetcode/String to Integer (atoi).cpp
@@ -27,3 +27,40 @@ public:
         return sign*res;
     }
 };
+
+
+/////////////
+class Solution {
+public:
+    int myAtoi(string str) {
+        int res = 0;
+        bool neg = false;
+        int n = str.size();
+        int idx = 0;
+        while (idx < n && str[idx] == ' ') {
+            idx++;
+        }
+        if (idx == n) {
+            return 0;
+        }
+        if (str[idx] == '+') {
+            idx++;
+        } else if (str[idx] == '-') {
+            neg = true;
+            idx++;
+        }
+        for (; idx < n && str[idx] >= '0' && str[idx] <= '9'; ++idx) {
+            if (res < INT_MAX/10 || (res == INT_MAX/10 && str[idx]-'0'<=7)) {
+                res = res * 10 + str[idx] - '0';
+            } else {
+                if (neg) return INT_MIN;
+                else return INT_MAX;
+            }
+        }
+        if (neg) {
+            return (0 - res);
+        }
+        return res;
+        
+    }
+};

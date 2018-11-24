6b7326b928697194aef47736daff86a54e76181f
baoxue <baoxue.zhao@gmail.com>
2018-11-24 21:00:03 +0800

    Update Pow(x, n).cpp

--- leetcode/Pow(x, n).cpp
+++ leetcode/Pow(x, n).cpp
@@ -18,3 +18,30 @@ public:
             return half*half*x;
     }
 };
+
+// divide and conquer
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (x == 0) {
+            return 0;
+        }
+        long long nn = n;
+        if (nn < 0) {
+            return fastPow(1.0/x, -nn);
+        }
+        return fastPow(x, nn);
+    }
+    
+    double fastPow(double x, long long n) {
+        if (n == 0) {
+            return 1;
+        }
+        double half = fastPow(x, n/2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+};

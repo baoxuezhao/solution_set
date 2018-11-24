4c41b1392f601c8d27f5af16c64a98127a151a87
baoxue <baoxue.zhao@gmail.com>
2018-11-24 13:32:02 +0800

    Update First Missing Positive.cpp

--- leetcode/First Missing Positive.cpp
+++ leetcode/First Missing Positive.cpp
@@ -33,3 +33,23 @@ public:
         return len+1;
     }
 };
+
+// new solution
+// http://www.cnblogs.com/grandyang/p/4395963.html
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i]-1]) {
+                swap(nums[i], nums[nums[i]-1]);
+            }
+        }
+        for (int i=0; i<n; i++) {
+            if (nums[i] != i+1) {
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+};

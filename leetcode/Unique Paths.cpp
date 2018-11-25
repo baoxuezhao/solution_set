c4b5e42e2e37dbb1c9189fa5b3fc7905088e0a28
baoxue <baoxue.zhao@gmail.com>
2018-11-25 17:55:39 +0800

    Update Unique Paths.cpp

--- leetcode/Unique Paths.cpp
+++ leetcode/Unique Paths.cpp
@@ -13,3 +13,20 @@ public:
         return dp[n-1];
     }
 };
+
+// 组合解法，注意溢出
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        return (int)combination(m+n-2, min(n-1,m-1));
+    }
+    
+    long long combination(int m, int n) {
+        long long times = 1;
+        for (int i=1; i<=n; ++i) {
+            times *= (m - i + 1);
+            times /= i;
+        }
+        return times;
+    }
+};

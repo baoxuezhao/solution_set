56a4fdc6dcb3fe7879d5a1239de9c57af356d54d
baoxue <baoxue.zhao@gmail.com>
2018-12-01 17:27:36 +0800

    Update Recover Binary Search Tree.cpp

--- leetcode/Recover Binary Search Tree.cpp
+++ leetcode/Recover Binary Search Tree.cpp
@@ -39,6 +39,41 @@ public:
         swap(brokens.first->val, brokens.second->val);
     }
     
+    // 思路：https://www.geeksforgeeks.org/fix-two-swapped-nodes-of-bst/
+    void recoverTree(TreeNode* root) {
+        std::pair<TreeNode*, TreeNode*> pair1(nullptr, nullptr);
+        std::pair<TreeNode*, TreeNode*> pair2(nullptr, nullptr);
+        TreeNode* prev = nullptr;
+        inorder(root, prev, pair1, pair2);
+        if (pair2.first == nullptr && pair2.second == nullptr) {
+            swap(pair1.first->val, pair1.second->val);
+        } else {
+            swap(pair1.first->val, pair2.second->val);
+        }
+        return;
+    }
+    
+    void inorder(TreeNode* root, TreeNode*& prev,
+        std::pair<TreeNode*, TreeNode*>& pair1, 
+        std::pair<TreeNode*, TreeNode*>& pair2) {
+        
+        if (root == nullptr) {
+            return;
+        }
+        inorder(root->left, prev, pair1, pair2);
+        if (prev != nullptr && root->val < prev->val) {
+            if (pair1.first == nullptr) {
+                pair1.first = prev;
+                pair1.second = root;
+            } else {
+                pair2.first = prev;
+                pair2.second = root;   
+            }
+        }
+        prev = root;
+        inorder(root->right, prev, pair1, pair2);
+    }
+    
     void visit(TreeNode *prev, TreeNode *cur, pair<TreeNode*, TreeNode*> &brokens)
     {
         if(prev->val <= cur->val) return;
